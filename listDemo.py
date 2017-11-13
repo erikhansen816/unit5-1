d72756c1e7ad86b63d0633808f3743b4e91adc3d
smeds1 <smeds1@users.noreply.github.com>
2017-11-13 14:28:07 -0500

    Updated from Brython Server: 11/13/2017 2:28:07 PM

--- listDemo.py
+++ listDemo.py
@@ -1,1 +1,12 @@
+#Sam Smedinghoff
+#11/13/17
+#listDemo.py - print out first and last words in a list
 
+words = input('Enter some words: ').split(' ')
+
+#print out the list one item per line
+for w in words:
+    print(w)
+
+print('The first word was', words[0])
+print('The last word was', words[-1])
